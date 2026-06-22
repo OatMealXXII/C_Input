3da14ebbe1ee38648c3d0da23ab49ce9dc3bd50c
OatMealXXII <oatbangping2@gmail.com>
2026-06-22 10:25:25 +0700

    Add cafe.c

---
+++ cafe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int time, sum, price = 15;
+    printf("Enter time you use : ");
+    scanf("%d", &time);
+    sum = time * price;
+    printf("Price for using %d hours : %d Bath", time ,sum);
+
+    return 0;
+}
