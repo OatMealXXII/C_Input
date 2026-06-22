08195059b9a00b5b7f56f1676cb393d23f0d0cd2
OatMealXXII <oatbangping2@gmail.com>
2026-06-22 10:58:34 +0700

    Fix sqaure2.c comment

--- sqaure2.c
+++ sqaure2.c
@@ -3,7 +3,7 @@
 int main() {
     // หาพื้นที่สี่เหลี่ยมผืนผ้า
     // ข้อมูลที่ต้องใช้ : กว้าง, ยาว, พื้นที่
-    // 2 x (กว้าง x ยาว)
+    // (กว้าง x ยาว)
     float width, height, space;
     printf("Enter width: ");
     scanf("%f", &width); // 17.2
